4e1bbbf5214fe4bc823822d88ff13099aa7c53cf
husan eshonqulov <husan.eshonqulov.00@gmail.com>
2022-05-01 23:02:08 +0500

    complete UI

---
+++ main.js
@@ -0,0 +1,10 @@
+function operate(op, a, b) {
+    switch (op) {
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/': return a / b;
+    }
+}
+
+console.log(operate('/', 2, 3));
