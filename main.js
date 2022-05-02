0ef2143a55ccc75975bf30bcdb78d446ed747478
husan eshonqulov <husan.eshonqulov.00@gmail.com>
2022-05-02 19:29:47 +0500

    add btn, ops, clear, delete

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+const currScreen = document.querySelector('#currentOperationScreen');
+const lastScreen = document.querySelector('#lastOperationScreen');
+const digitBtns = document.querySelectorAll('.digit');
+const clearBtn = document.querySelector('#clearBtn');
+const deleteBtn = document.querySelector('#deleteBtn');
+const opBtns = document.querySelectorAll('.operation');
+
+digitBtns.forEach(digBtn => digBtn.addEventListener('click', () => handleDigit(digBtn)));
+opBtns.forEach(opBtn => opBtn.addEventListener('click', () => handleOperation(opBtn)));
+clearBtn.addEventListener('click', () => currScreen.textContent = 0);
+deleteBtn.addEventListener('click', () => currScreen.textContent = currScreen.textContent.slice(0, -1));
+
+function handleDigit(digBtn) {
+    const digit = digBtn.textContent;
+    const num = currScreen.textContent;
+    currScreen.textContent = num !== '0' ? num + digit : digit;
+}
+
+function handleOperation(opBtn) {
+    const op = opBtn.textContent;
+    const a = Number(currScreen.textContent);
+    lastScreen.textContent = a + ' ' + op;
+}
+
 function operate(op, a, b) {
     switch (op) {
         case '+': return a + b;
@@ -7,4 +31,3 @@ function operate(op, a, b) {
     }
 }
 
-console.log(operate('/', 2, 3));
